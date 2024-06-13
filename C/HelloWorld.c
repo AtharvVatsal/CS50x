1e6839edee2780cf961c24f14020c64a51ecae5e
Atharv Vatsal <atharvvatsal@outlook.com>
2024-06-13 14:03:31 +0530

    Hello World Program In C

---
+++ C/HelloWorld.c
@@ -0,0 +1,6 @@
+#include <stdio.h>    //Headser File
+
+int main() {    //main function, execution occurs here
+    printf("Hello World!\n");    //output function in stdio.h
+    return 0;     //return 0 to main function, can use void data type aswell
+}
